780a392ccb255a33d694a283ea114fd973ef8fb3
pavan kothapally <85874797+kothapallypavan@users.noreply.github.com>
2022-09-03 21:47:21 +0530

    Update App.js

--- loan/src/App.js
+++ loan/src/App.js
@@ -1,11 +1,13 @@
 import Register from './components/Register';
 import Form from "./components/Form"
 import "./components/form.css"
+import Dashboared from "./components/Dashboard"
+
 function App() {
 
   return (
     <main className="App">
-      <Form />
+      <Dashboared />
     </main>
   );
 }
